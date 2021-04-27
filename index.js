86e8797d79634c530376f74b7f7eed31559e7240
Andrey <andrevbnk72@gmail.com>
2021-04-27 14:40:24 +0300

    Refactor v2

--- index.js
+++ index.js
@@ -81,8 +81,9 @@ const getIndexList = async html =>{
  });
  return href;
 }
-const iterator = [...Array(365)].map((elem,i)=>i).slice(26,365)
-(async function(){
+const iterator = [...Array(365)].map((elem,i)=>i).slice(26,365);
+
+;(async function(){
     for(let pageCount of iterator){
         await sleep(180000);
         console.log(urlTelecrack+pageCount);
